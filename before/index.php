3702153c288e7052260da5862e3074602b9e6561
ridholess <newridho1298@gmail.com>
2026-04-01 12:00:14 +0700

    add before week-7-responsi-si

---
+++ before/index.php
@@ -0,0 +1,48 @@
+<?php
+require 'koneksi.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Responsi Week 7</title>
+    <!-- Tailwind Css -->
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <!-- Font Styles -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Montserrat+Alternates:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet">
+    <style>
+        body {
+            background-color: #F7EDE8;
+            font-family: 'Montserrat Alternates', sans-serif;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="flex flex-col justify-center items-center h-screen">
+        <div class="w-2/3">
+            <h1 class="text-6xl text-center font-bold text-[#CD4631]">Responsi - Week 7</h1>
+            <p class="text-md mt-6 text-center font-light"><span class="font-semibold italic">Goodluck!! </span>Lorem
+                ipsum
+                dolor sit amet
+                consectetur adipisicing
+                elit. Cupiditate
+                soluta asperiores dolore illum. Provident, odio perferendis?</p>
+            <div class="w-full flex justify-center mt-6">
+                <a href="login.php"
+                    class="bg-[#000] shadow-[6px_6px_0px_0px_rgba(205,70,49,0.80)] hover:shadow-[0px_0px_0px_0px] transition-all duration-300 ease-in-out px-16 py-3 text-xl font-medium rounded-md text-white">Mulai</a>
+            </div>
+
+        </div>
+    </div>
+</body>
+
+</html>
